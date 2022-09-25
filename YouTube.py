2954160d61ed4a33b0e9b1a3d14cdc50b3c7e789
DONALDBZR <andygaspard@hotmail.com>
2022-09-25 15:04:39 +0400

    [UPDATE 1.3.3] Refactoring the YTD

--- YouTube.py
+++ YouTube.py
@@ -4,42 +4,63 @@ from pytube import YouTube
 from Database import Database
 # YTD class
 class YTD:
-    def __init__(self, link: str, category: str):
-        # Instance of YouTube's API
-        # TYPE: YouTube
-        self.video = YouTube(link)
-        # Artist of the video
+    def __init__(self):
+        # The url of the video
         # TYPE: String
-        self.__artist = self.video.title.split(" - ")[0]
-        # Title of the video
+        self.__link = ""
+        # Category of the video
         # TYPE: String
-        self.__title = self.video.title.split(" - ")[1]
+        self.__category = ""
         # The database handler
         # TYPE: Database
         self._Database = Database()
-        # Category of the video
+        # Artist of the video
         # TYPE: String
+        self.__artist = ""
+        # Title of the video
+        # TYPE: String
+        self.__title = ""
+
+    def getLink(self):
+        return self.__link
+    def setLink(self, link: str):
+        self.__link = link
+
+    def getCategory(self):
+        return self.__category
+    def setCategory(self, category: str):
         self.__category = category
-        # Stream of the media
-        # TYPE: Stream
-        self._stream = self.video.streams.get_highest_resolution()
-        # Verifying the song exists in the database
-        self.verifyTitle()
+
+    def getArtist(self):
+        return self.__artist
+    def setArtist(self, artist: str):
+        self.__artist = artist
+
+    def getTitle(self):
+        return self.__title
+    def setTitle(self, title: str):
+        self.__title = title
+
     def verifyTitle(self) -> str | None:
-        parameters = (self.__artist, self.__title)
-        self._Database.query("SELECT * FROM YouTubeDownloader.Downloads WHERE DownloadsArtist = %s OR DownloadsTitle = %s", parameters)
+        self.video = YouTube(self.getLink())
+        self.setArtist(self.video.title.split(" - ")[0])
+        self.setTitle(self.video.title.split(" - ")[1])
+        parameters = (self.getArtist(), self.getTitle())
+        self._Database.query("SELECT * FROM YouTubeDownloader.Downloads WHERE DownloadsArtist = %s AND DownloadsTitle = %s", parameters)
         if len(self._Database.resultSet()) > 0:
             return "The song already exists"
         else:
             # Verifying the category of the media content
             self.verifyCategory()
+
     def verifyCategory(self):
-        if self.__category == "Music":
+        if self.getCategory() == "Music":
             self._stream = self.video.streams.get_audio_only()
         else:
             self._stream = self.video.streams.get_by_itag(22)
         # Downloading the media content
         self.download()
+
     def download(self) -> str:
         self._stream.download('C:\Apache24\htdocs\ytd.local\Videos')
         return "Media file downloaded!"
